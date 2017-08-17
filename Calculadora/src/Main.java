7da0e60b0cc083134eeb6d3e61255c3f08dc5516
solgalain94 <solgalain@gmail.com>
2017-08-17 08:30:24 -0300

    Commit subir archivos

---
+++ Calculadora/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		VentanaCalculadora calculadora = new VentanaCalculadora();
+		calculadora.setVisible(true);
+	}
+}
